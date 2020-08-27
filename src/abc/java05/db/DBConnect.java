69d0b9f266480f805c35c27eadbafb8f44726048
RinhBK <58002770+RinhBK@users.noreply.github.com>
2020-08-27 09:43:03 +0700

    add DBconnect

--- src/abc/java05/db/DBConnect.java
+++ src/abc/java05/db/DBConnect.java
@@ -1,6 +1,8 @@
 package abc.java05.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnect {
     final static private String url =   "jdbc:sqlserver://localhost:1433" +
@@ -13,5 +15,17 @@ public class DBConnect {
     // khoi tao ket noi
     public DBConnect () {
 
+    }
+
+    public static Connection getConnection()  {
+        try {
+            Class.forName("com.sqlserver.jdbc.Driver");
+            return DriverManager.getConnection(url,usernameDB,passwordDB);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
