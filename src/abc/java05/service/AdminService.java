319a7e77706d519fd08fcc284fbae351fa929d25
Nguyen Duy Tuong <tuong261099@gmail.com>
2020-08-28 08:42:33 +0700

    edit admin service

--- src/abc/java05/service/AdminService.java
+++ src/abc/java05/service/AdminService.java
@@ -2,12 +2,10 @@ package abc.java05.service;
 
 import abc.java05.db.DBBook;
 import abc.java05.model.Book;
-
-import java.util.List;
 import java.util.Scanner;
 
 public class AdminService {
-    private DBBook dbBook = new DBBook();
+//    private final DBBook dbBook = new DBBook();
     /*
     * Tao 1 sach, goi thang db cung dc
     * Neu sach da ton tai tra ve false, ko add dc
@@ -27,10 +25,7 @@ public class AdminService {
         System.out.println("Enter the content: ");
         String content = scanner.nextLine();
         Book book = new Book(title,author,brief,publisher,category,content);
-        if(dbBook.save(book)){
-            return true;
-        }
-        return false;
+        return DBBook.save(book);
     }
     /*
     * Xoa 1 q sach
@@ -38,9 +33,9 @@ public class AdminService {
     * Sach ko ton tai tra ve false
     * */
     public boolean deleteBook(String bookID) {
-        Book book = dbBook.getBookByID(bookID);
+        Book book = DBBook.getBookByID(bookID);
         if(book != null){
-            return dbBook.delete(book.getId());
+            return DBBook.delete(book.getId());
         }
         return false;
     }
@@ -50,10 +45,10 @@ public class AdminService {
     * Sach ko ton tai tra ve false
     * */
     public boolean updateBook(String bookID, String bookContent) {
-        Book book = dbBook.getBookByID(bookID);
+        Book book = DBBook.getBookByID(bookID);
         if(book != null){
             book.setContent(bookContent);
-            return dbBook.save(book);
+            return DBBook.save(book);
         }
         return false;
     }
