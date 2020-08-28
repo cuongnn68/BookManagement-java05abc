9686b225820bf225d1529814b12a746861b55795
cuongnn <nncuong.nnc@gmail.com>
2020-08-28 14:14:08 +0700

    start working on GUI

---
+++ src/abc/java05/GUIApp.java
@@ -0,0 +1,16 @@
+package abc.java05;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIApp  extends Application {
+    static public void run(String... args) {
+
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
