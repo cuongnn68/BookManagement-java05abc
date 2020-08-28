8c34a7632187a0fe696cd43ae8caa197179401b7
Chien <fbalchien@gmail.com>
2020-08-28 21:53:44 +0700

    dbUser and dbBookcase

--- src/abc/java05/db/DBUser.java
+++ src/abc/java05/db/DBUser.java
@@ -1,7 +1,11 @@
 package abc.java05.db;
 
 import abc.java05.model.User;
-
+import abc.java05.util.Role;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBUser {
@@ -10,15 +14,57 @@ public class DBUser {
     * Lay danh sach toan bo user
     * */
     public List<User> getAll() {
-        return null;
+    	List<User> listUser= new ArrayList<User>();
+    	String sql="SELECT u.UserID,u.user_name,u.password,u.book_case_id,r.authority FROM [User] u JOIN [Role] r ON  u.UserID=r.UserID";
+    	try {
+    		Connection conn=DBConnect.getConnection();
+    		PreparedStatement ps=conn.prepareStatement(sql);
+    		ResultSet rs=ps.executeQuery();
+    		while(rs.next()) {
+    			User user= new User();
+    			user.setId(rs.getString("UserID"));
+    			user.setUserName(rs.getString("user_name"));
+    			user.setPassword(rs.getString("password"));
+    			user.setBookCaseID(rs.getString("book_case_id"));
+    			user.setRole(Role.setRole(rs.getInt("authority")));
+    			listUser.add(user);
+    		}
+    		ps.close();
+    		conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return listUser;
     }
 
     /*
     * Lay user theo username
     * Tra ve null neu ko co nguoi dung nao
     * */
-    public User get(String username) {
-        return null;
+    @SuppressWarnings("null")
+	public User get(String username) {
+    	String sql="SELECT u.UserID,u.user_name,u.password,u.book_case_id, r.authority FROM [User] u JOIN [Role] r ON u.UserID=r.UserID  WHERE user_name="+username;
+    	User user= null;
+    	try {
+    		Connection conn=DBConnect.getConnection();
+    		PreparedStatement ps=conn.prepareStatement(sql);
+    		ResultSet rs=ps.executeQuery();
+    		while(rs.next()) {
+    			user.setId(rs.getString("UserID"));
+    			user.setUserName(rs.getString("user_name"));
+    			user.setPassword(rs.getString("password"));
+    			user.setBookCaseID(rs.getString("book_case_id"));
+    			user.setRole(Role.setRole(rs.getInt("authority")));
+    		}
+    		ps.close();
+    		conn.close();
+    		
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+    	return user;
     }
 
 //    //ko dung, ko lam
--- src/abc/java05/util/Role.java
+++ src/abc/java05/util/Role.java
@@ -1,5 +1,18 @@
 package abc.java05.util;
 
 public enum Role {
-    USER, ADMIN
+	USER(1), ADMIN(0);
+	 private int value;
+    Role(int value) {
+        this.value = value;
+    }
+    public static Role setRole(int value) {
+   	 if(value==0) {
+   		 return Role.ADMIN;
+   	 }
+   	 else if(value==1){
+   		 return Role.USER;
+   	 }
+   	 return null;
+    }
 }
