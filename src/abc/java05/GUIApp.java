aa1033f50960b30625bfe690985d5f04748f47ec
cuongnn <nncuong.nnc@gmail.com>
2020-08-29 18:37:53 +0700

    complete login view

--- src/abc/java05/GUIApp.java
+++ src/abc/java05/GUIApp.java
@@ -1,16 +1,24 @@
 package abc.java05;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GUIApp  extends Application {
     static public void run(String... args) {
-
+        launch(args);
     }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+        primaryStage.setTitle("Test title login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
 
     }
 }
